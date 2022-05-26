0b25fb10abe8d9ea88b5f6be8d74c0a8a9eab874
segundocarlos <segundocarlos@hotmail.com>
2022-05-26 01:26:58 -0500

    Primera integracion
    Lectura de Post y Categorias

---
+++ packages/my-coupons-theme/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Root from './Root';
+
+
+
+
+
+export default {
+  name: "my-coupons-theme",
+  roots: {
+    theme: Root
+  },
+  "state": {
+      "theme": {}
+  },
+  actions: {
+    theme: {}
+  }
+};
